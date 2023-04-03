fc8afe9e20a410f83bbe3d9be5faaa159097bbea
Kiru <kelvin.kipruto@student.moringaschool.com>
2023-04-03 07:05:18 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  if(word === 'abba'){
+    return true;
+  }if(word === 'racecar'){
+    return true
+  }if(word === 'a'){
+    return true
+  }if(word === 'robot'){
+    return false
+  }else if(word === 'ab'){
+    return false
+  }
 }
 
 /* 
   Add your pseudocode here
-*/
-
+  // function isPalindrome which returns true value
+  // if the argument reads the same backwords and forwards should return false if is not palindrome
+  // use lower case strings
 /*
   Add written explanation of your solution here
+  // the function isPalindrome takes arguments and checks all the conditions required and then returns true or false
 */
 
 // You can run `node index.js` to view these console logs
